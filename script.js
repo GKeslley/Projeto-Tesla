f313b8411e508c70c870a079b3441e2b5d69a61c
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-07-20 19:16:31 -0300

    Implementando a sidebar

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import initAccordionList from "./javascript/accordionList.js";
 import initAnimaNumbers from "./javascript/animaNumbers.js";
 import initAnimationScroll from "./javascript/scroll.js";
+import initSidebar from "./javascript/sidebar.js";
 
 initAnimationScroll();
 initAccordionList();
 initAnimaNumbers();
+initSidebar();
