57a392c940144254bdb7f84549a85eb78ce63629
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-07-20 12:24:07 -0300

    Implementação ao anima números

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import initAccordionList from "./javascript/accordionList.js";
+import initAnimaNumbers from "./javascript/animaNumbers.js";
 import initAnimationScroll from "./javascript/scroll.js";
 
 initAnimationScroll();
 initAccordionList();
+initAnimaNumbers();
