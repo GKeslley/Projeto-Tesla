c101e41f8b7e378bcca8312962538b6d90452830
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-07-24 17:38:03 -0300

    Implementando design da página sobre

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ import initAccordionList from "./javascript/accordionList.js";
 import initAnimaNumbers from "./javascript/animaNumbers.js";
 import initAnimationScroll from "./javascript/scroll.js";
 import initSidebar from "./javascript/sidebar.js";
+import initSobre from "./javascript/sobre.js";
 
 initAnimationScroll();
 initAccordionList();
 initAnimaNumbers();
 initSidebar();
+initSobre();
